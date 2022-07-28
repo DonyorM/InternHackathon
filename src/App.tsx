cfe4eabdf60176debf3f0e7dc62265c2b0207e5d
Daniel Manila <daniel.manila@willowtreeapps.com>
2022-07-28 10:49:08 -0400

    Firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { app } from "./firebase";
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    signInWithEmailAndPassword(
+      auth,
+      "daniel.manila@willowtreeapps.com",
+      "password"
+    );
+  }, []);
+
+  console.log(loading, ";", user);
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaSzAHu8bNB6n9V979vH25rQH8wqFnOzg",
+
+  authDomain: "internhackathon-65db4.firebaseapp.com",
+
+  projectId: "internhackathon-65db4",
+
+  storageBucket: "internhackathon-65db4.appspot.com",
+
+  messagingSenderId: "467438240234",
+
+  appId: "1:467438240234:web:1b598e0450580dceb7b293",
+
+  measurementId: "G-3S0VFYNZZV",
+};
+
+export const app = initializeApp(firebaseConfig);
