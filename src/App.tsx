6e9ea7ac2b36fe5c576a6aead9264491e5f5010b
Andrew Byerle <abyerle@gmail.com>
2022-07-28 14:07:58 -0400

    working keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Searchbar from "./components/Searchbar/Searchbar";
 import KeyboardComponent from "./components/keyboard/KeyboardComponent";
 import { app } from "./firebase";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -12,6 +12,8 @@ const auth = getAuth(app);
 
 function App() {
   const [user, loading, error] = useAuthState(auth);
+
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     signInWithEmailAndPassword(
@@ -24,10 +26,13 @@ function App() {
   console.log(loading, ";", user);
 
   return (
-    <div>
-      <Searchbar />
-      <KeyboardComponent />
+    <div style={{ height: "100vh" }}>
+      <Searchbar searchInput={searchInput} />
       <TileSection itemIds={[1024, 1027, 1065]} />
+      <KeyboardComponent
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+      />
     </div>
   );
 }
--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,10 +1,13 @@
 import styles from "./SearchBar.module.css";
 import React, { Component, useRef } from "react";
 
-const Searchbar: React.FC = () => {
+interface SearchbarProps {
+  searchInput: string;
+}
+const Searchbar: React.FC<SearchbarProps> = ({ searchInput }) => {
   return (
     <div className={styles.searchBar}>
-      <input type="text" />
+      <input type="text" value={searchInput} />
     </div>
   );
 };
